b0067e1d797cbd9a1c81ff6ef2827ddc8b83559c
Witul-thawon <w02tvr02gt@gmail.com>
2026-08-31 10:18:20 +0700

    add header finish

--- Lab02/Program.cs
+++ Lab02/Program.cs
@@ -35,6 +35,87 @@ namespace Lab02
             Console.WriteLine("===== BOSS STATUS: AFTER DAMAGE =====");
             Console.WriteLine($"HP: {currentHp}  / {maxHp}");
             Console.WriteLine($"HP Percent 47%");
+
+            Console.WriteLine("======PART B: HERO NAMES=======/n");
+            string heroName = "Armmy";
+            int heroHp = 110;
+            float heroAtk = 18;
+            double heroCritRate = 16.5;
+            char heroRank = 'B';
+            Console.WriteLine();
+            Console.WriteLine("/n____HERO NO. 1____");
+            Console.WriteLine($"Name: {heroName}");
+            Console.WriteLine($"HP: {heroHp}");
+            Console.WriteLine($"ATK: {heroAtk}");
+            Console.WriteLine($"CRIT RATE: {heroCritRate}");
+            Console.WriteLine($"RANK: {heroRank}");
+
+            string AssasinName = "Weir";
+            int AssasinHp = 120;
+            float AssasinAtk = 20;
+            double AssasinCritRate = 19.5;
+            char AssasinRank = 'A';
+            Console.WriteLine();
+            Console.WriteLine("/n____HERO NO. 2____");
+            Console.WriteLine($"AssasinName: {AssasinName}");
+            Console.WriteLine($"AssasinHP: {AssasinHp}");
+            Console.WriteLine($"AssasinATK: {AssasinAtk}");
+            Console.WriteLine($"AssasinCRIT RATE: {AssasinCritRate}");
+            Console.WriteLine($"AssasinRANK: {AssasinRank}");
+
+            string CarryName = "Myja";
+            int CarryHp = 150;
+            float CarryAtk = 25;
+            double CarryCritRate = 22.5;
+            char CarryRank = 'S';
+            Console.WriteLine();
+            Console.WriteLine("/n____HERO NO. 3____");
+            Console.WriteLine($"CarryName: {CarryName}");
+            Console.WriteLine($"CarryHP: {CarryHp}");
+            Console.WriteLine($"CarryATK: {CarryAtk}");
+            Console.WriteLine($"CarryCRIT RATE: {CarryCritRate}");
+            Console.WriteLine($"CarryRANK: {CarryRank}");
+
+            string FighterName = "Farm";
+            int FighterHp = 200;
+            float FighterAtk = 16;
+            double FighterCritRate = 20;
+            char FighterRank = 'C';
+            Console.WriteLine();
+            Console.WriteLine("/n____HERO NO. 4____");
+            Console.WriteLine($"FighterName: {FighterName}");
+            Console.WriteLine($"FighterHP: {FighterHp}");
+            Console.WriteLine($"FighterATK: {FighterAtk}");
+            Console.WriteLine($"FighterCRIT RATE: {FighterCritRate}");
+            Console.WriteLine($"FighterRANK: {FighterRank}");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         }
+
     }
 }
